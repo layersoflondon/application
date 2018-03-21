d9c3fdec95fb588fbdc64fc699aa08fd7b85ad4b
Paul Hendrick <paul@errorstudio.co.uk>
2018-03-21 11:35:01 +0000

    map#show route

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  root 'map#show'
+
   resources :records, only: %i[index create show update destroy] do
     resources :images, controller: 'record_images', only: %i[index create show update destroy]
     resources :documents, controller: 'record_documents', only: %i[index create show update destroy]
     resources :attachments, controller: 'record_attachments', only: %i[index create show update destroy]
   end
+
   resources :collections, only: %i[index create show update destroy] do
     resources :records, controller: 'collection_records', only: %i[index create destroy]
   end
-  resources :teams, only: %i[index create show update destroy]end
+
+  resources :teams, only: %i[index create show update destroy]
+end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_03_20_122900) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_03_20_122900) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_03_20_122900) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "attachable_id"
     t.string "attachable_type"
     t.string "caption"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2018_03_20_122900) do
     t.index ["record_id"], name: "index_attachments_on_record_id"
   end
 
-  create_table "collection_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "collection_records", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.bigint "collection_id"
     t.bigint "record_id"
     t.datetime "created_at", null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2018_03_20_122900) do
     t.index ["record_id"], name: "index_collection_records_on_record_id"
   end
 
-  create_table "collections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "collections", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "owner_type"
@@ -69,18 +69,18 @@ ActiveRecord::Schema.define(version: 2018_03_20_122900) do
     t.index ["owner_type", "owner_id"], name: "index_collections_on_owner_type_and_owner_id"
   end
 
-  create_table "data_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "data_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "file_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "file_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "like_count", default: 0
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 2018_03_20_122900) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
