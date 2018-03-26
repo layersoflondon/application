5fa05a242663e6b094bf83441c2052be2b800622
Jordi <jordi@error.agency>
2018-03-26 12:34:59 +0200

    Fix styles

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
- devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
- has_many :records, dependent: :nullify
+  has_many :records, dependent: :nullify
 
- # TODO - users should change the state of their records before being deleted.
- # before_destroy do
- #  records.each {|r| r.make_orphan! }
- # end
-
+  # TODO: - users should change the state of their records before being deleted.
+  # before_destroy do
+  #  records.each {|r| r.make_orphan! }
+  # end
 end
