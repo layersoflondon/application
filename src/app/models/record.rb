337d62bde4a3a24e5ef2dd589f20a814947a6143
Jordi <jordi@error.agency>
2018-04-17 15:49:56 +0200

    change event names

--- src/app/models/record.rb
+++ src/app/models/record.rb
@@ -1,30 +1,34 @@
 class Record < ApplicationRecord
   include AASM
 
-  enum state: %i[drafted published pending_review flagged]
+  enum state: %i[draft published pending_review flagged deleted]
 
   aasm column: :state, enum: true do
-    state :drafted, initial: true
+    state :draft, initial: true
     state :published
     state :pending_review
     state :flagged
+    state :deleted
 
-    event :draft do
-      transitions from: %i[], to: :drafted
+    event :mark_as_draft do
+      transitions from: %i[draft], to: :draft
     end
 
-    event :review do
-      transitions from: %i[drafted flagged], to: :pending_review
+    event :mark_as_pending_review do
+      transitions from: %i[draft flagged pending_review], to: :pending_review
     end
 
-    event :publish do
-      transitions from: %i[pending_review], to: :published
+    event :mark_as_published do
+      transitions from: %i[pending_review published], to: :published
     end
 
-    event :flag do
-      transitions from: %i[pending_review published], to: :flagged
+    event :mark_as_flagged do
+      transitions from: %i[pending_review published flagged], to: :flagged
     end
 
+    event :mark_as_deleted do
+      transitions from: %i[draft published pending_review flagged], to: :deleted
+    end
 
   end
 
@@ -38,15 +42,15 @@ class Record < ApplicationRecord
   # state by default. See https://github.com/aasm/aasm
   # enum state: %i[draft published pending_review flagged]
 
-  validates :title, :state, presence: true, if: -> { state == 'drafted' }
-  validates :title, :description, :state, :lat, :lng, :date, :location, presence: true, if: -> { state != 'drafted' }
-  validates :title, length: { in: 3..255 }, if: -> { state != 'drafted' }
-  validates :description, length: { minimum: 3 }, if: -> { state != 'drafted' }
-  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, if: -> { state != 'drafted' }
-  validates :lng, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, if: -> { state != 'drafted' }
-  validates_format_of :date, with: /\d{4}-\d{2}-\d{2}/, message: '^Date must be in the following format: yyyy-dd-mm', if: -> { state != 'drafted' }
+  validates :title, :state, presence: true, if: -> { state == 'draft' }
+  validates :title, :description, :state, :lat, :lng, :date, :location, presence: true, if: -> { state != 'draft' }
+  validates :title, length: { in: 3..255 }, if: -> { state != 'draft' }
+  validates :description, length: { minimum: 3 }, if: -> { state != 'draft' }
+  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, if: -> { state != 'draft' }
+  validates :lng, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, if: -> { state != 'draft' }
+  validates_format_of :date, with: /\d{4}-\d{2}-\d{2}/, message: '^Date must be in the following format: yyyy-dd-mm', if: -> { state != 'draft' }
   # TODO: is there a standard rails validator for dates? not sure.
-  validate :date_is_in_the_past, if: -> { state != 'drafted' }
+  validate :date_is_in_the_past, if: -> { state != 'draft' }
 
   validates :user, presence: true # , unless: ->(r) {r.orphan?}
 
