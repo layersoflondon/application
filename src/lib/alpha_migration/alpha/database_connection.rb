3f9457a76a1afe1a95d073161752df0947f2028e
Ed Jones <ed@errorstudio.co.uk>
2018-06-12 23:44:54 +0100

    move alpha database config into its own file

--- src/lib/alpha_migration/alpha/database_connection.rb
+++ src/lib/alpha_migration/alpha/database_connection.rb
@@ -2,7 +2,7 @@ module Alpha
   module DatabaseConnection
     extend ActiveSupport::Concern
 
-    DB = YAML::load(ERB.new(File.read(Rails.root.join("config","database.yml"))).result)["legacy"]
+    DB = YAML::load(ERB.new(File.read(Rails.root.join("config","legacy.yml"))).result)
 
     included do
       establish_connection DB
