b5dc5941da60a9ca0dc2743e9079a827925f4322
Ed Jones <ed@errorstudio.co.uk>
2018-06-15 17:19:42 +0100

    local files in dev; need to figure out how to render from s3 via cloudflare

--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.cache_store = config.cache_store = :redis_store, 'redis://redis:6379/0/cache'
     
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
