d60fdc880f2631a1c3d1b0031344f19887497fe3
Ed Jones <ed@errorstudio.co.uk>
2018-06-21 17:29:32 +0100

    add a couple of todos on search to document how we need to render collections

--- src/app/controllers/search_controller.rb
+++ src/app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   skip_after_action :verify_authorized, only: [:index]
 
   def index
+    # TODO we need to query collections and records, and union the 2, sorting by score (at least for now, we might be able to get ES to do better than that)
     if params[:q].present?
       @records = Record.custom_search(params)
     elsif params[:geobounding].present?
