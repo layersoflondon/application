a55bff98f56d2caadea2f7758b5c9a6df4f5ed92
Ed Jones <ed@errorstudio.co.uk>
2018-06-21 16:23:13 +0100

    redirect to a param provided

--- src/app/controllers/users/sessions_controller.rb
+++ src/app/controllers/users/sessions_controller.rb
@@ -1,27 +1,16 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super do
+      session[:return_to] = params[:return_to]
+    end
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  private
+  def after_sign_in_path_for(user)
+    return_to = session[:return_to]
+    session.delete(:return_to)
+    return_to || root_path
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
