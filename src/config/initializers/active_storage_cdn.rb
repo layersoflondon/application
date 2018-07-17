7baf46ac8bcaa6acc414a86040c538b7a82257ea
Ed Jones <ed@errorstudio.co.uk>
2018-07-17 13:41:19 +0100

    only cache the image *redirect* for 5 mins

--- src/config/initializers/active_storage_cdn.rb
+++ src/config/initializers/active_storage_cdn.rb
@@ -1,5 +1,5 @@
 Rails.application.config.to_prepare do
   # Although private, the cache control on the asset redirection will last in the browser for 2 weeks.
-  ActiveStorage::Blob.service.url_expires_in = 2.weeks
+  ActiveStorage::Blob.service.url_expires_in = 5.minutes
 end
 
