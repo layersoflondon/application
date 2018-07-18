e3bf35da304f36b32fd81a049c4ad7e1aee6cf31
Ed Jones <ed@errorstudio.co.uk>
2018-07-18 11:17:15 +0100

    ensure partially-visible cards are rendered

--- src/app/frontend/components/card.js
+++ src/app/frontend/components/card.js
@@ -59,7 +59,7 @@ import VisibilitySensor from 'react-visibility-sensor';
     };
 
     return (
-      <VisibilitySensor onChange={visibilityChanged}>
+      <VisibilitySensor onChange={visibilityChanged} partialVisibility={true}>
         <Link to={path} className={container_classes} onMouseEnter={this.highlightCard.bind(this)} onMouseOut={()=>this.props.card.highlighted=false}>
           <div className="wrapper">
             <div className="image" style={image_styles}>
