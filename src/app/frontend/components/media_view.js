d5dc66559eab63fb1640def431804a298e42838c
Paul Hendrick <paul@errorstudio.co.uk>
2018-07-18 11:50:42 +0100

    wrap media view in router with key for transition

--- src/app/frontend/components/media_view.js
+++ src/app/frontend/components/media_view.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react';
 import {inject, observer} from "mobx-react";
 import {Link, withRouter} from 'react-router-dom';
-import MediaItem from './media_item';
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
+
+import { Route } from 'react-router';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 @inject('routing', 'trayViewStore')
 @withRouter
@@ -24,33 +26,37 @@ import {TransitionGroup, CSSTransition} from 'react-transition-group';
       </div>;
     });
 
-    return <TransitionGroup>
-      <CSSTransition timeout={3000} classNames={'media-item'} key={`${Math.random()}`} >
-        <div className="m-overlay is-showing" style={{zIndex: 12341234, top: 0, left: 0, border: '2px solid red'}}>
-          <div className="s-overlay--media is-showing">
-            <div className="m-media-viewer *m-media-viewer--basic">
-              <div className="close">
-                <Link to={`/map/records/${this.props.match.params.id}`}>Close</Link>
-              </div>
-              <div className="wrap">
-                {this.props.children}
-
-                <div className="m-media-viewer-thumbs">
-                  <div className="controls">
-                    <button className="scroll-left"></button>
-                    <button className="scroll-right"></button>
+    return <Router>
+      <Route render={( {location} ) => console.log(location) || (
+        <TransitionGroup>
+          <CSSTransition timeout={3000} classNames={'media-item'} key={location.key} >
+            <div className="m-overlay is-showing" style={{zIndex: 12341234, top: 0, left: 0, border: '2px solid red'}}>
+              <div className="s-overlay--media is-showing">
+                <div className="m-media-viewer *m-media-viewer--basic">
+                  <div className="close">
+                    <Link to={`/map/records/${this.props.match.params.id}`}>Close</Link>
                   </div>
-                  <div className="pane">
+                  <div className="wrap">
+                    {this.props.children}
 
-                    {media_list}
+                    <div className="m-media-viewer-thumbs">
+                      <div className="controls">
+                        <button className="scroll-left"></button>
+                        <button className="scroll-right"></button>
+                      </div>
+                      <div className="pane">
 
+                        {media_list}
+
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-    </CSSTransition>
-  </TransitionGroup>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
+    </Router>
   }
 }
