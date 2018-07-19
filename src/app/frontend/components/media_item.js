efd72ff28f481d955b043c48ea168bfd7598c9e2
Paul Hendrick <paul@errorstudio.co.uk>
2018-07-19 16:34:01 +0100

    audio player uses uploaded file

--- src/app/frontend/components/media_item.js
+++ src/app/frontend/components/media_item.js
@@ -26,13 +26,9 @@ import MediaElement from './media_element';
     console.log(`Showing soundcloud()`);
     // return <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/249483615&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     // const sources = [{src: 'http://localhost:3000/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb0FVIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--40a994ee9320f2c255848f88e185bbe910f11cec/Audio%20file%20mp3', type: 'audio/mpeg'}];
-    const sources = [
-        {src: 'http://www.streambox.fr/playlists/test_001/stream.m3u8', type: 'application/x-mpegURL'},
-        {src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4', type: 'video/mp4'},
-        {src: 'rtmp://firehose.cul.columbia.edu:1935/vod/mp4:sample.mp4', type: 'video/rtmp'}
-      ], config = {}, tracks = {};
+    const sources = [{src: this.state.media_item.attachable.url, type: this.state.media_item.attachable.content_type}], config = {}, tracks = {};
 
-    return <MediaElement width='100%' height='100%' id='player' sources={[{src: 'http://localhost:3000/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb0FVIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--40a994ee9320f2c255848f88e185bbe910f11cec/Audio%20file%20mp3', type: 'audio/mpeg'}]} tracks={tracks} options={config} mediaType='audio' />
+    return <MediaElement width='100%' height='100%' id='player' sources={sources} tracks={tracks} options={config} mediaType='audio' />
   }
 
   video() {
@@ -41,7 +37,6 @@ import MediaElement from './media_element';
   }
 
   render() {
-    console.log(this.state.media_item, this.state.media_item.media_type);
     const render_method = this.state.media_item.media_type;
     const in_gallery_view = this.props.trayViewStore.record.view_type === 'gallery';
 
@@ -55,13 +50,21 @@ import MediaElement from './media_element';
 
             {in_gallery_view && (
               <div className="meta">
-                <div className="attribution">
-                  <p>Duis dapibus mollis erat ac.</p>
-                </div>
-                <div className="caption">
-                  <p>Proin ornare sapien in nunc fermentum euismod. Sed lectus purus, ornare vel faucibus volutpat, pharetra vitae nisl. Nunc metus neque, dictum sit amet risus eget, porttitor tincidunt purus. Fusce ultricies est sed vulputate fermentum. Nunc vel tristique orci. Proin dapibus…</p>
-                  <button className="show-all">Read more</button>
-                </div>
+                {this.state.media_item.credit && (
+                  <div className="attribution">
+                    <p dangerouslySetInnerHTML={{__html: this.state.media_item.credit}}></p>
+                  </div>
+                )}
+
+                {this.state.media_item.caption && (
+                  <div className="caption">
+                    <p>
+                      <span dangerouslySetInnerHTML={{__html: this.state.media_item.caption}}></span>
+                    </p>
+
+                    <button className="show-all">Read more</button>
+                  </div>
+                )}
               </div>
             )}
           </div>
