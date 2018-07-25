077eafffc84f0fd4c7e2f26f8ce97d1a6b2d7523
Ed Jones <ed@errorstudio.co.uk>
2018-07-25 21:17:14 +0100

    make paths in account mailer more idiomatic

--- src/app/mailers/account_mailer.rb
+++ src/app/mailers/account_mailer.rb
@@ -3,8 +3,8 @@ class AccountMailer < ApplicationMailer
   def team_join_request(user, team, key)
     @user = user
     @team = team
-    @url_accept = url_for(controller: 'teams', action: 'accept_request', id: @team.id, key: key)
-    @url_deny = url_for(controller: 'teams', action: 'deny_request', id: @team.id, key: key)
+    @url_accept = accept_request_to_team_url(id: @team.id, key: key)
+    @url_deny = reject_request_to_team_url(id: @team.id, key: key)
     mail(to: @user.email, subject: 'Layers of London - Request to join a team')
   end
 
@@ -12,7 +12,7 @@ class AccountMailer < ApplicationMailer
     @user = user
     @user_invited = user_invited
     @team = team
-    @url_accept = url_for(controller: 'teams', action: 'accept_invitation', id: @team.id, key: key)
+    @url_accept = accept_invitation_to_team_url(id: @team.id, key: key)
     mail(to: @user_invited.email, subject: 'Layers of London - Invitation to join a team')
   end
 end
