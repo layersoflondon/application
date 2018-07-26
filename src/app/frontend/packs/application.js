78f5881578adf2fe17849caa092b8ecb31928983
Ed Jones <ed@errorstudio.co.uk>
2018-07-26 15:44:30 +0100

    Revert "add babel polyfill"
    This reverts commit 86d8c36c719321da1fdde250224e21b541740c30.

--- src/app/frontend/packs/application.js
+++ src/app/frontend/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import 'babel-polyfill';
 import React from 'react'
 import ReactDOM from 'react-dom'
 
