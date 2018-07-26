bc89dd4031402cefd72eb38b69567ba5e0c508ba
Ed Jones <ed@errorstudio.co.uk>
2018-07-26 23:28:10 +0100

    add route and template for article

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
   skip_after_action :verify_authorized, only: [:show]
   decorates_assigned :posts, :post, with: PostDecorator
 
+  layout 'templates/article'
+
   def show
     @post = Post.find_by(slug: params[:id]).first
     raise Rooftop::RecordNotFoundError, "Post not found" unless @post.present?
