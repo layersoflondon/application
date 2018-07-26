bbc8df1c38d16d78123cb5d61c32fb1bc4c611ca
Ed Jones <ed@errorstudio.co.uk>
2018-07-26 22:58:00 +0100

    deploy production from master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ role :web, %w{deployment@lol-web01.hosts.errorstudio.com}
 role :db, %w{deployment@lol-web01.hosts.errorstudio.com}
 
 # Git branch
-set :branch, 'development'
+set :branch, 'master'
 
 #the base domain for this site - is appended to the primary domain for a prelaunch url
 set :base_domain, "prelaunch.error.agency"
