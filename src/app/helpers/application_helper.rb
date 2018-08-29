f136251303b80ac6af129da3275f249bccec9cda
Ed Jones <ed@errorstudio.co.uk>
2018-08-29 16:40:40 +0100

    render placeholder image in record and collection json, so it's always available in the react application references #303

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def activestorage_url_for(resource)
     Rails.application.routes.url_helpers.polymorphic_url(resource)
   end
+
+  def placeholder_image
+    url = image_url("placeholder/wide-%02d.svg" % rand(1..10))
+    Rails.configuration.x.image_variants.keys.collect(&:to_s).inject({}) {|h, k| h[k] = url; h}
+  end
 end
