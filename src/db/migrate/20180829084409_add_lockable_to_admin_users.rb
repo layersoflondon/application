9a102cd75971f95693de60d819204154ff4dcd8a
Ed Jones <ed@errorstudio.co.uk>
2018-08-29 09:51:13 +0100

    add locking strategy (5 attempts) to admin user

---
+++ src/db/migrate/20180829084409_add_lockable_to_admin_users.rb
@@ -0,0 +1,11 @@
+class AddLockableToAdminUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_table :admin_users do |t|
+      ## Lockable
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+
+    end
+  end
+end
