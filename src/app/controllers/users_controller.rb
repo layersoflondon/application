6bc87a401d61365fa1b8b401ab5f732ff05b4fbc
Ed Jones <ed@errorstudio.co.uk>
2018-08-31 16:23:59 +0100

    Sideload user's records in /users/n endpoint

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
   skip_after_action :verify_authorized, only: [:show]
   decorates_assigned :user, with: UserDecorator
+  decorates_assigned :records, with: RecordDecorator
   def show
     @user = User.find(params[:id])
+    @records = RecordsIndex.user_records(
+      user_id: @user.id,
+      record_states: (current_user.try(:id) == @user.id.to_i) ? ['published', 'draft'] : ['published']
+    )
   end
 end
