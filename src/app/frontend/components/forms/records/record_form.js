86af29ba4f943fd036db1502813bb95e4831cb4a
Ed Jones <ed@errorstudio.co.uk>
2018-09-05 11:57:48 +0100

    Don't allow record form to render without having chosen a lat/lng.

--- src/app/frontend/components/forms/records/record_form.js
+++ src/app/frontend/components/forms/records/record_form.js
@@ -17,6 +17,11 @@ import RecordModel from './../../../models/record';
   }
 
   componentWillMount() {
+
+    if (!this.props.recordFormStore.record.lat || !this.props.recordFormStore.record.lng) {
+      this.props.router.push('/map/choose-place');
+    }
+
     if( this.props.trayViewStore.record ) {
       // duplicate the record object so that the one we're mutating isn't what may be visible in the tray (its observed
       // attributes will change in real-time, but aren't actually persisted which might be confusing...)
