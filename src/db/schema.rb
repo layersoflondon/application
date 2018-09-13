2019e2402b97c6758035b15d02441c92b30deb50
Ed Jones <ed@errorstudio.co.uk>
2018-09-13 20:49:20 +0100

    Add contributing user to CollectionRecord join table so we can ensure people can't remove records added by others

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_195633) do
+ActiveRecord::Schema.define(version: 2018_09_13_073840) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "namespace"
@@ -151,6 +151,7 @@ ActiveRecord::Schema.define(version: 2018_09_06_195633) do
     t.bigint "record_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "contributing_user_id"
     t.index ["collection_id"], name: "index_collection_records_on_collection_id"
     t.index ["record_id"], name: "index_collection_records_on_record_id"
   end
@@ -192,7 +193,7 @@ ActiveRecord::Schema.define(version: 2018_09_06_195633) do
     t.integer "image_id"
   end
 
-  create_table "record_reports", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "record_reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "record_id"
     t.string "issue", null: false
     t.string "message", null: false
