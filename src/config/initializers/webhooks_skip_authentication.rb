2b49e667cf565f8922d32da64a6be93288a28aa6
Paul Hendrick <paul@errorstudio.co.uk>
2018-09-18 16:34:16 +0100

    move map zoom controls

--- src/config/initializers/webhooks_skip_authentication.rb
+++ src/config/initializers/webhooks_skip_authentication.rb
@@ -1,5 +1,5 @@
-Rails.application.config.to_prepare do
-  Rooftop::Rails::WebhooksController.send(:skip_before_action, :authenticate_user!)
-  Rooftop::Rails::WebhooksController.send(:skip_after_action, :verify_authorized)
-  Rooftop::Rails::PreviewController.send(:skip_after_action, :verify_authorized)
-end
+# Rails.application.config.to_prepare do
+#   Rooftop::Rails::WebhooksController.send(:skip_before_action, :authenticate_user!)
+#   Rooftop::Rails::WebhooksController.send(:skip_after_action, :verify_authorized)
+#   Rooftop::Rails::PreviewController.send(:skip_after_action, :verify_authorized)
+# end
