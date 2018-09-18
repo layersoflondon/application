3d3fe8ff7b79b0f01a6c1d160ba6f6979b853474
Ed Jones <ed@errorstudio.co.uk>
2018-09-18 10:47:55 +0100

    WIP on collections picker

--- src/app/frontend/components/collection_picker.js
+++ src/app/frontend/components/collection_picker.js
@@ -24,35 +24,10 @@ import RecordModel from "../models/record";
       record: this.props.record,
       collections: []
     };
+   
   }
 
   componentWillMount() {
-
-    // observe the record's collection IDs
-    this.observerDisposer = observe(this.props.record, 'collection_ids', (change) => {
-      //  We need to persist the collections at this point - hit the RecordCollections endpoint
-
-      const added_ids = change.newValue.filter((id) => {return change.oldValue.indexOf(id) < 0});
-      const removed_ids = change.oldValue.filter((id) => {return change.newValue.indexOf(id) < 0});
-      if (added_ids.length) {
-        Record.add_to_collections(this.id, {collection_ids: added_ids}).then((result) => {
-          this.state.record = new RecordModel.fromJS(result);
-          console.log("new collections: ",this.state.record.collection_ids.toJS() )
-        }).catch((errors) => {
-          console.log(errors);
-        });
-      }
-
-      if (removed_ids.length) {
-        console.log('removing', removed_ids);
-        Record.remove_from_collections(this.id, {collection_ids: removed_ids}).then((result) => {
-          this.state.record = new RecordModel.fromJS(result);
-          console.log("new collections: ",this.state.record.collection_ids.toJS() )
-        }).catch((errors) => {
-          console.log(errors);
-        });
-      }
-    });
 
     // Get a list of all collections owned by this user, and by others (which are allowed to be added to)
       //the API returns all collections except those owned by the user.
@@ -80,11 +55,41 @@ import RecordModel from "../models/record";
   }
 
   componentWillReceiveProps() {
+
     this.setState({
       enabled_user_collections: this.props.record.user_collections,
       enabled_everyone_collections: this.props.record.everyone_collections,
       record: this.props.record
     });
+
+    // observe the record's collection IDs
+    window.record = this.props.record;
+    this.observerDisposer = observe(this.props.record, 'collection_ids', (change) => {
+      console.log(change);
+      //  We need to persist the collections at this point - hit the RecordCollections endpoint
+      const added_ids = change.newValue.filter((id) => {return change.oldValue.indexOf(id) < 0});
+      const removed_ids = change.oldValue.filter((id) => {return change.newValue.indexOf(id) < 0});
+      if (added_ids.length) {
+        Record.add_to_collections(this.id, {collection_ids: added_ids}).then((result) => {
+          // this.state.record = new RecordModel.fromJS(result);
+          console.log("new collections: ",this.state.record.collection_ids.toJS() )
+        }).catch((errors) => {
+          console.log(errors);
+        });
+      }
+
+      if (removed_ids.length) {
+        console.log('removing', removed_ids);
+        Record.remove_from_collections(this.id, {collection_ids: removed_ids}).then((result) => {
+          // this.state.record = new RecordModel.fromJS(result);
+          console.log("new collections: ",this.state.record.collection_ids.toJS() )
+        }).catch((errors) => {
+          console.log(errors);
+        });
+      }
+    });
+    
+
 
   }
 
@@ -94,71 +99,85 @@ import RecordModel from "../models/record";
     this.setState({showing: collection_set});
   }
 
-  handleSelectOnChange(options, event) {
+  handleSelectOnChange(option, event) {
     let {action} = event;
+    // when the select option changes, we need to add it to the list of collection ids for the record and remove it from the select
+
+    //
+    let updated_collections = this.state[`enabled_${this.state.showing}`].slice();
+    if( action === 'select-option' ) {
+      if(updated_collections.indexOf(option)<0) {
+        updated_collections.push(option);
+        this.props.record.collection_ids.push(option.value);
+      }
+    }
+
+    this.setState({[`enabled_${this.state.showing}`] : updated_collections});
+
+    
+
     // don't need to run this code if the select options are just being switched between personal and everyone collections
 
-    console.log(action);
-    if (action !== 'set-value') {
-
-      let updated_collections = this.state[`enabled_${this.state.showing}`].slice();
-      let collection_ids = this.state.record.collection_ids.slice();
-
-      if( action === 'clear' ) {
-        updated_collections = [];
-      }else if( action === 'remove-value' ) {
-        const removed_collections = updated_collections.filter((c) => options.indexOf(c)<0);
-        const removed_collection_ids = removed_collections.map((c) => c.value);
-
-        collection_ids = collection_ids.filter((i) => removed_collection_ids.indexOf(i)<0);
-        updated_collections = options;
-      }else if( action === 'select-option' ) {
-        console.log(options);
-        options.map((option) => {
-          if(updated_collections.indexOf(option)<0) {
-            updated_collections.push(option);
-            collection_ids.push(option.value);
-          }
-        });
-
-      }
-
-      this.state.record.collection_ids = collection_ids;
-      this.setState({[`enabled_${this.state.showing}`] : updated_collections});
-    }
+    // console.log(action);
+    // if (action !== 'set-value') {
+    //
+    //   let updated_collections = this.state[`enabled_${this.state.showing}`].slice();
+    //   let collection_ids = this.state.record.collection_ids.slice();
+    //
+    //   if( action === 'clear' ) {
+    //     updated_collections = [];
+    //   }else if( action === 'remove-value' ) {
+    //     const removed_collections = updated_collections.filter((c) => options.indexOf(c)<0);
+    //     const removed_collection_ids = removed_collections.map((c) => c.value);
+    //
+    //     collection_ids = collection_ids.filter((i) => removed_collection_ids.indexOf(i)<0);
+    //     updated_collections = options;
+    //   }else if( action === 'select-option' ) {
+    //     console.log(options);
+    //     options.map((option) => {
+    //       if(updated_collections.indexOf(option)<0) {
+    //         updated_collections.push(option);
+    //         collection_ids.push(option.value);
+    //       }
+    //     });
+    //
+    //   }
+    //
+    //   this.state.record.collection_ids = collection_ids;
+    //   this.setState({[`enabled_${this.state.showing}`] : updated_collections});
+    // }
   }
 
   removeFromCollections(event) {
     event.preventDefault();
     let {value, name} = event.target;
 
-    let updated_collections = this.state[`enabled_${name}`].slice();
-    const removed = updated_collections.find((c) => c.value === parseInt(value, 10) );
-
-    let index = updated_collections.indexOf(removed);
-    if( index>-1 ) {
-      updated_collections.splice(index, 1);
-    }
-
-    index = this.state.record.collection_ids.indexOf( parseInt(value, 10 ) );
-    if( index>-1 ) {
-      const collection_ids = this.state.record.collection_ids.slice();
-      collection_ids.splice(index, 1);
-      this.state.record.collection_ids = collection_ids;
-    }
-
-    this.setState({[`enabled_${name}`]: updated_collections});
-
-    if( this.state.showing === name ) {
-      setTimeout(() => {
-        this.selectRef.current.select.setValue(updated_collections);
-      }, 1);
-    }
+    // let updated_collections = this.state[`enabled_${name}`].slice();
+    // const removed = updated_collections.find((c) => c.value === parseInt(value, 10) );
+    //
+    // let index = updated_collections.indexOf(removed);
+    // if( index>-1 ) {
+    //   updated_collections.splice(index, 1);
+    // }
+    //
+    // index = this.state.record.collection_ids.indexOf( parseInt(value, 10 ) );
+    // if( index>-1 ) {
+    //   const collection_ids = this.state.record.collection_ids.slice();
+    //   collection_ids.splice(index, 1);
+    //   this.state.record.collection_ids = collection_ids;
+    // }
+    //
+    // this.setState({[`enabled_${name}`]: updated_collections});
+    //
+    // if( this.state.showing === name ) {
+    //   setTimeout(() => {
+    //     this.selectRef.current.select.setValue(updated_collections);
+    //   }, 1);
+    // }
   }
 
   render() {
 
-    window.record = this.props.record;
 
     const toggled_classname = (this.state.showing === "user_collections") ? "" : "is-toggled";
     const collection_options = this.props.collectionStore[this.state.showing].values().map((c) => ({value: c.id, label: c.title}));
@@ -176,7 +195,7 @@ import RecordModel from "../models/record";
           </label>
         </div>
 
-        <Select placeholder='' options={collection_options} hideSelectedOptions={true} isMulti={true} searchable={true} onChange={this.handleSelectOnChange.bind(this)} closeMenuOnSelect={true} ref={this.selectRef}/>
+        <Select placeholder='' options={collection_options} hideSelectedOptions={true} isMulti={false} searchable={true} onChange={this.handleSelectOnChange.bind(this)} closeMenuOnSelect={true} ref={this.selectRef}/>
 
       </div>
 
