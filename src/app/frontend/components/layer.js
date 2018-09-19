d73d744800c98c7ad0b56e1f40f29833b84e69b7
Paul Hendrick <paul@errorstudio.co.uk>
2018-09-19 12:00:08 +0100

    hide the menu when clicking a menu item

--- src/app/frontend/components/layer.js
+++ src/app/frontend/components/layer.js
@@ -6,7 +6,6 @@ import Parser from 'html-react-parser';
 @observer export default class Layer extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props.layer.title, this.props.layer.credit)
   }
 
   handleOnClick(event) {
