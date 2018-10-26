df7f6842fba3d4395fc67d0e5cba0932aa3f2353
Ed Jones <ed@error.agency>
2018-10-26 15:48:32 +0100

    Validate that a url is actually a url

--- src/app/models/attachments/url.rb
+++ src/app/models/attachments/url.rb
@@ -4,7 +4,7 @@ class Attachments::Url < ApplicationRecord
 
   # before_validation :add_http_to_url
 
-  validates_format_of :url, with: URI::DEFAULT_PARSER.regexp[:ABS_URI]
+  validate :url_must_be_valid
 #   this model will respond to url() because there's a column in the database
 
   # Utility method called when we're indexing this attachment type.
@@ -20,6 +20,17 @@ class Attachments::Url < ApplicationRecord
     unless self.url.match URI::DEFAULT_PARSER.regexp[:ABS_URI]
       self.url = "http://" + self.url
     end
+  end
+
+  def url_must_be_valid
+    begin
+      unless URI.parse(url).kind_of?(URI::HTTP)
+        errors.add(:url, "The link URL isn't in an accepted format")
+      end
+    rescue URI::InvalidURIError
+      errors.add(:url, "The link URL isn't in an accepted format")
+    end
+    
   end
   
 end
