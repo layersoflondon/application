691e4fdfafa3ed078b987a4b4720125e396fe365
Ed Jones <ed@error.agency>
2019-03-08 16:25:15 +0000

    Add TeamPicker component to RecordForm - WIP

--- src/app/frontend/components/forms/records/record_form.js
+++ src/app/frontend/components/forms/records/record_form.js
@@ -8,6 +8,7 @@ import Dates from './dates';
 import Media from './media';
 import Links from './links';
 import CollectionsEditor from './collections_editor';
+import TeamPicker from './team_picker';
 import Record from './../../../sources/record';
 import RecordModel from './../../../models/record';
 import NotFound from "../../not_found";
@@ -205,7 +206,7 @@ import NotFound from "../../not_found";
                 <Media {...this.props} />
                 <Links {...this.props} />
                 <CollectionsEditor {...this.props} />
-                {/*<Team {...this.props} />*/}
+                <TeamPicker {...this.props} />
               </div>
 
               <div className="form-actions">
--- src/app/frontend/components/forms/records/team_picker.js
+++ src/app/frontend/components/forms/records/team_picker.js
@@ -1,21 +1,77 @@
 import React,{Component} from 'react';
 import RecordFormComponentState from './record_form_component_state';
 import {observer} from "mobx-react";
+import Team from "../../../sources/team";
+import Select from 'react-select';
 
 @observer class TeamPicker extends Component {
   constructor(props) {
     super(props);
+
+    this.selectRef = React.createRef();
+    this.state = {
+      record: this.props.recordFormStore.record,
+      allow_team_editing: false,
+
+    }
+  }
+
+  componentWillMount() {
+    let teams = [];
+    Team.index().then((response) => {
+      teams = response.data.map((team) => ({value: team.id, label: team.name}));
+      this.setState({teams: teams});
+    });
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      record: this.props.recordFormStore.record,
+      allow_team_editing: this.props.recordFormStore.record.allow_team_editing
+    })
+  }
+
+  handleAllowTeamEditingOnChange(event) {
+    this.setState({allow_team_editing: event.target.checked});
+  }
+
+  handleSelectOnChange(option, event) {
+    let {action} = event;
+
+    // if a select option changes, we need to add the id to the records collection ids (which are observed in the RecordModel and persisted)
+    if( action === 'select-option' ) {
+      this.state.record.team_id = option.value;
+    }
+
   }
 
   render() {
     const pane_styles = {display: this.props.recordFormStore.visible_pane==='team' ? 'block' : 'none'};
     const pane_classname = (this.props.recordFormStore.visible_pane==='team') ? 'is-open' : '';
+    const toggled_classname = (this.state.allow_team_editing) ? "is-toggled" : "";
+
     return (
       <div className={`section section--assign-to-team ${pane_classname}`}>
-        <h2 className="title" data-name="team" onClick={this.togglePaneVisibility}>Assign to a team</h2>
+        <h2 className="title" data-name="team" onClick={this.togglePaneVisibility}>Who can edit?</h2>
 
         <div className="pane" style={pane_styles}>
-          todo
+          <div className="m-assign-to-team">
+            <div className="form">
+              <div className={`form-group form-group--toggle-switch ${toggled_classname}`}>
+                <label>
+                  <span>Only you</span>
+                  <input type="checkbox" name='showing_collections' checked={this.state.allow_team_editing} onChange={this.handleAllowTeamEditingOnChange.bind(this)}/>
+                  <span className="toggle"></span>
+                  <span>You and your team</span>
+                </label>
+              </div>
+              {
+                this.state.allow_team_editing &&
+                <Select placeholder='Choose a collection…' options={this.state.teams} value="" isMulti={false} searchable={true} onChange={this.handleSelectOnChange.bind(this)} closeMenuOnSelect={false} ref={this.selectRef}/>
+              }
+
+            </div>
+          </div>
         </div>
 
       </div>
@@ -23,4 +79,4 @@ import {observer} from "mobx-react";
   }
 }
 
-export default RecordFormComponentState.bindComponent(Team);
+export default RecordFormComponentState.bindComponent(TeamPicker);
