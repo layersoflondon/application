cb8c1fd26f62ec7f51c86e861264298c958c4267
Ed Jones <ed@error.agency>
2019-03-08 16:23:46 +0000

    remove list of collections from teams endpoint - slow and unused

--- src/app/frontend/components/forms/records/team_picker.js
+++ src/app/frontend/components/forms/records/team_picker.js
@@ -2,16 +2,16 @@ import React,{Component} from 'react';
 import RecordFormComponentState from './record_form_component_state';
 import {observer} from "mobx-react";
 
-@observer class Team extends Component {
+@observer class TeamPicker extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     const pane_styles = {display: this.props.recordFormStore.visible_pane==='team' ? 'block' : 'none'};
-
+    const pane_classname = (this.props.recordFormStore.visible_pane==='team') ? 'is-open' : '';
     return (
-      <div className="section">
+      <div className={`section section--assign-to-team ${pane_classname}`}>
         <h2 className="title" data-name="team" onClick={this.togglePaneVisibility}>Assign to a team</h2>
 
         <div className="pane" style={pane_styles}>
--- src/app/views/search/_team.json.jbuilder
+++ src/app/views/search/_team.json.jbuilder
@@ -1,6 +1,3 @@
 json.id team.id
 json.name team.name
 json.description team.description || ""
-json.records do
-  json.array! team.collections.collect {|r| OpenStruct.new(r)}.select {|r| RecordPolicy.new(current_user, r).show?}, partial: 'search/collection', as: :collection
-end
