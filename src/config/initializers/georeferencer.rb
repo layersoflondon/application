0a995cfeb7a3a1a82da46f36d2b5f97ca09ed365
Ed Jones <ed@error.agency>
2019-03-15 13:59:53 +0000

    Move georeferencer functionality into a gem

---
+++ src/config/initializers/georeferencer.rb
@@ -0,0 +1,9 @@
+Georeferencer.configure do |config|
+  if Rails.env.development?
+    config.proxy = "https://localhost:9998"
+    config.ssl_options = {
+      verify: false
+    }
+  end
+  config.subdomain = 'layersoflondon'
+end
