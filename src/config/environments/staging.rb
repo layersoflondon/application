298a7eb899bdb077b14f38aadf080f1bd87ab85e
Ed Jones <ed@error.agency>
2019-03-28 08:41:48 +0000

    point staging to the right place for redis

--- src/config/environments/staging.rb
+++ src/config/environments/staging.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.cache_store = :redis_store, 'redis://127.0.0.1:6379/1/cache'
+  config.cache_store = :redis_store, 'redis://db01.lol:6379/1/cache'
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
