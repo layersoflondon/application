656179b0d6ec1cd475e1aae554d17d44abd3ff89
Ed Jones <ed@error.agency>
2019-04-05 11:13:54 +0100

    Add admin action to view georeferencer project on site

--- src/app/admin/georeferencer_collections.rb
+++ src/app/admin/georeferencer_collections.rb
@@ -2,6 +2,10 @@ ActiveAdmin.register Georeferencer::Project do
 
   permit_params :name, :description, :georeferencer_id, :show_on_website
 
+  action_item :view_on_website, only: [:show, :edit] do
+    link_to("View on website (new window)", georeferencer_project_path(resource.slug), target: "_blank")
+  end
+
   form do |f|
     f.inputs do
       f.input :name
