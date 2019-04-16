aee6e136c966b77f2de22a650d413d55264dfb63
Ed Jones <ed@error.agency>
2019-04-16 15:34:18 +0100

    Dynamically select featured collections / records on the homepage highlight form

--- src/app/admin/featured_items.rb
+++ src/app/admin/featured_items.rb
@@ -10,11 +10,9 @@ ActiveAdmin.register FeaturedItem, as: "Homepage Highlight" do
 
 
   controller do
-    def update
-      update! do |f|
-        f.html {redirect_to edit_admin_team_path(params[:id])}
-      end
-    end
+   def show
+     redirect_to edit_admin_homepage_highlight_path(params[:id])
+   end
   end
 
   index do
@@ -39,7 +37,12 @@ ActiveAdmin.register FeaturedItem, as: "Homepage Highlight" do
 
 
   form do |f|
-
+    f.inputs do
+      f.input :item_type, as: :radio, collection: %w(Record Collection)
+      f.input :item, as: :searchable_select, collection: Record.published.order(:title), input_html: {class: "record-options", disabled: f.object.item_type.in?(["Collection", nil])}
+      f.input :item, as: :searchable_select, collection: Collection.public_read.order(:title), input_html: {class: "collection-options", disabled: f.object.item_type.in?(["Record", nil])}
+    end
+    f.actions
   end
 
 end
