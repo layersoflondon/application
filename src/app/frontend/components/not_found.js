ff41f43648d42929c022749e0e91da89af639e59
Ed Jones <ed@error.agency>
2019-04-18 13:05:11 +0100

    Add sitemap generator

--- src/app/frontend/components/not_found.js
+++ src/app/frontend/components/not_found.js
@@ -1,5 +1,6 @@
-import React,{Component} from 'react';
+import React,{Component, Fragment} from 'react';
 import {observer} from "mobx-react";
+import Helmet from 'react-helmet';
 
 import {inject} from "mobx-react/index";
 
@@ -15,18 +16,25 @@ import {inject} from "mobx-react/index";
   }
 
   render() {
-    return <div className='m-overlay'>
-      <div className="close">
-        <a href="#" className="close" onClick={this.handleCloseOnClick.bind(this)}>Close</a>
-      </div>
+    return (
+      <Fragment>
+        <Helmet>
+          <title>Content not found</title>
+          <meta name='robots' content='noindex,nofollow' />
+        </Helmet>
+        <div className='m-overlay'>
+          <div className="close">
+            <a href="#" className="close" onClick={this.handleCloseOnClick.bind(this)}>Close</a>
+          </div>
 
-      <div className="m-add-record">
-        <h1>Content not found</h1>
+          <div className="m-add-record">
+            <h1>Content not found</h1>
 
-        <p>
-          There is no content at this address. If you think this is a mistake please <a href="/contact-us">get in touch</a>
-        </p>
-      </div>
-    </div>
+            <p>
+              There is no content at this address. If you think this is a mistake please <a href="/contact-us">get in touch</a>
+            </p>
+          </div>
+        </div>
+      </Fragment>)
   }
 }
