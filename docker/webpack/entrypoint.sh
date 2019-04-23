79e61ba5b6e4324658c870372c71e309b007df91
Ed Jones <ed@error.agency>
2019-04-23 13:58:13 +0100

    Local development in docker container

--- docker/webpack/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#BUNDLE_PATH=/gems
-# install yarn dependencies
-bundle check || bundle install
-yarn install
-# launch the server
-bundle exec ./bin/webpack-dev-server
-#while true; do sleep 1; done
