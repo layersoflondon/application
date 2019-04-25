db32f38760b99456d382d96bec0eac056dadf8f2
Andrew Wilkinson <andy@error.agency>
2019-04-25 11:11:49 +0100

    new layer tools fed

--- src/app/frontend/components/layer_tools_container.js
+++ src/app/frontend/components/layer_tools_container.js
@@ -60,6 +60,111 @@ const getListStyle = isDraggingOver => ({
         <div className="layers">
           {this.props.layersStore.activeLayers.values().map((l, index) => <span key={index}></span>)}
 
+          <div>
+            <div className="layer has-components is-open">
+
+              <span className="name">Layer Alpha</span>
+              <div class="view-controls">
+                <span className="show-hide"></span>
+                <span className="slider">
+                  <div className="rc-slider">
+                    <div className="rc-slider-rail"></div>
+                    <div className="rc-slider-track"></div>
+                    <div className="rc-slider-step"></div>
+                    <div role="slider" tabindex="0" aria-valuemin="0" aria-valuemax="1" aria-valuenow="1" aria-disabled="false" className="rc-slider-handle"></div>
+                    <div className="rc-slider-mark"></div>
+                  </div>
+                </span>
+              </div>
+
+              <div class="layer-components">
+                <div class="layer-component">
+                  <span className="key-symbol key-symbol--outline key-symbol--red"></span>
+                  <span className="name">Parishes</span>
+                  <div class="view-controls">
+                    <span className="show-hide"></span>
+                    <span className="slider">
+                      <div className="rc-slider">
+                        <div className="rc-slider-rail"></div>
+                        <div className="rc-slider-track"></div>
+                        <div className="rc-slider-step"></div>
+                        <div role="slider" tabindex="0" aria-valuemin="0" aria-valuemax="1" aria-valuenow="1" aria-disabled="false" className="rc-slider-handle"></div>
+                        <div className="rc-slider-mark"></div>
+                      </div>
+                    </span>
+                  </div>
+                 </div>
+                <div class="layer-component">
+                  <span className="key-symbol key-symbol--outline key-symbol--green"></span>
+                  <span className="name">Map image</span>
+                  <div class="view-controls">
+                    <span className="show-hide"></span>
+                    <span className="slider">
+                      <div className="rc-slider">
+                        <div className="rc-slider-rail"></div>
+                        <div className="rc-slider-track"></div>
+                        <div className="rc-slider-step"></div>
+                        <div role="slider" tabindex="0" aria-valuemin="0" aria-valuemax="1" aria-valuenow="1" aria-disabled="false" className="rc-slider-handle"></div>
+                        <div className="rc-slider-mark"></div>
+                      </div>
+                    </span>
+                  </div>
+                </div>
+                <div class="layer-component is-hidden">
+                  <span className="key-symbol key-symbol--outline key-symbol--orange"></span>
+                  <span className="name">Street network</span>
+                  <div class="view-controls">
+                    <span className="show-hide"></span>
+                    <span className="slider">
+                      <div className="rc-slider">
+                        <div className="rc-slider-rail"></div>
+                        <div className="rc-slider-track"></div>
+                        <div className="rc-slider-step"></div>
+                        <div role="slider" tabindex="0" aria-valuemin="0" aria-valuemax="1" aria-valuenow="1" aria-disabled="false" className="rc-slider-handle"></div>
+                        <div className="rc-slider-mark"></div>
+                      </div>
+                    </span>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className="layer">
+              <span className="name">Layer Beta</span>
+                <div class="view-controls">
+                    <span className="show-hide"></span>
+                    <span className="slider">
+                      <div className="rc-slider">
+                        <div className="rc-slider-rail"></div>
+                        <div className="rc-slider-track"></div>
+                        <div className="rc-slider-step"></div>
+                        <div role="slider" tabindex="0" aria-valuemin="0" aria-valuemax="1" aria-valuenow="1" aria-disabled="false" className="rc-slider-handle"></div>
+                        <div className="rc-slider-mark"></div>
+                      </div>
+                    </span>
+                </div>
+            </div>
+
+            <div className="layer">
+              <span className="name">Layer Gamma</span>
+                <div class="view-controls">
+                    <span className="show-hide"></span>
+                    <span className="slider">
+                      <div className="rc-slider">
+                        <div className="rc-slider-rail"></div>
+                        <div className="rc-slider-track"></div>
+                        <div className="rc-slider-step"></div>
+                        <div role="slider" tabindex="0" aria-valuemin="0" aria-valuemax="1" aria-valuenow="1" aria-disabled="false" className="rc-slider-handle"></div>
+                        <div className="rc-slider-mark"></div>
+                      </div>
+                    </span>
+                </div>
+            </div>
+
+          </div>
+
           <DragDropContext onDragEnd={this.onDragEnd.bind(this)}>
             <Droppable droppableId="droppable">
               {(provided, snapshot) => (
