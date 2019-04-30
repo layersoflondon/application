f454c2bb559184294c0f10f13eebf36bafd030a8
Ed Jones <ed@error.agency>
2019-04-30 16:32:50 +0100

    more docker in development

--- src/config/initializers/chewy.rb
+++ src/config/initializers/chewy.rb
@@ -2,5 +2,5 @@ Chewy.strategy(:atomic)
 Chewy.logger = Logger.new(STDOUT)
 Chewy.settings = {
   prefix: Rails.env,
-  host: Rails.env.development? ? "127.0.0.1:9200" : "db01.lol:9200"
+  host: Rails.env.development? ? "elasticsearch:9200" : "db01.lol:9200"
 }
