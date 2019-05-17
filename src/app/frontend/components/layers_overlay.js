6a8aa2c6b25b467131d442e80e88b3a4c8971845
Andrew Wilkinson <andy@error.agency>
2019-05-17 17:12:20 +0100

    layer picker done button fed

--- src/app/frontend/components/layers_overlay.js
+++ src/app/frontend/components/layers_overlay.js
@@ -56,6 +56,10 @@ import Equalizer from "./Equalizer";
                 </Equalizer>
               </div>
 
+              <div className="confirm">
+                <button>I'm done!</button>
+              </div>
+
             </div>
           </div>
         </div>
