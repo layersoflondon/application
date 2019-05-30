6fb19bbe0afeae1afb9ceff276de300e5c25cac4
Paul Hendrick <paul@errorstudio.co.uk>
2019-05-30 15:12:44 +0100

    fix migration order

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_104704) do
+ActiveRecord::Schema.define(version: 2019_05_02_124411) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "namespace"
