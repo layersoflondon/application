e6d5c3e0d59e4083660cabdb0b2c64f9f0d8a446
Andy Wilkinson <andy@error.agency>
2019-06-06 13:14:00 +0100

    blank images for layer pickers

--- src/app/frontend/components/layer_group.js
+++ src/app/frontend/components/layer_group.js
@@ -15,7 +15,7 @@ import {Link} from "react-router-dom";
     return <div className={`layer ${(this.props.layerGroup.is_active) ? "layer is-selected" : ""}`}>
       <Link to={`/map/layers/${this.props.layerGroup.slug}`}>
           {this.props.layerGroup.image &&
-          <div className="image" style={{'backgroundImage': 'url(' + this.props.layerGroup.image.card + ')'}}>
+          <div className="image" style={{'background': '#383838 url(' + this.props.layerGroup.image.card + ') center/cover'}}>
           </div>
           }
         <h2>{this.props.layerGroup.name}</h2>
