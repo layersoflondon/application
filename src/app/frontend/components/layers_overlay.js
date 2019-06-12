e4a4e5380d8f1ec17b246a6540419487a531b00b
Andy Wilkinson <andy@error.agency>
2019-06-12 15:28:00 +0100

    no results msg for layer picker

--- src/app/frontend/components/layers_overlay.js
+++ src/app/frontend/components/layers_overlay.js
@@ -18,7 +18,7 @@ import Layer from '../sources/layer';
     this.filter = () => {
       this.setState({searching: true});
       Layer.search(this.state).then((response) => {
-        const ids = response.data.map((i)=>i.id);
+        const ids = response.data.map((i) => i.id);
         this.setState({ids: ids, searching: false});
       });
     };
@@ -34,7 +34,7 @@ import Layer from '../sources/layer';
   }
 
   handleReturn(event) {
-    if(event.key === 'Enter' && this.state.searching === false) {
+    if (event.key === 'Enter' && this.state.searching === false) {
       this.filter();
     }
   }
@@ -49,8 +49,8 @@ import Layer from '../sources/layer';
     let className = "m-overlay";
     if (this.props.mapViewStore.overlay === 'layers') className += " is-showing";
 
-    const highlightedLayers = this.props.layersStore.highlightedLayerGroups.filter((layer_group)=>layer_group.inFilter(this.state.ids));
-    const layerDirectoryLayers = this.props.layersStore.layerGroups.filter((layer_group)=>layer_group.inFilter(this.state.ids));
+    const highlightedLayers = this.props.layersStore.highlightedLayerGroups.filter((layer_group) => layer_group.inFilter(this.state.ids));
+    const layerDirectoryLayers = this.props.layersStore.layerGroups.filter((layer_group) => layer_group.inFilter(this.state.ids));
 
     return (
       <Fragment>
@@ -70,12 +70,13 @@ import Layer from '../sources/layer';
               <div className="header">
                 <h1>Layers</h1>
                 <div className="search">
-                  <input placeholder="Search layers" type="text" name="search_layers" value={this.state.query} onKeyUp={this.handleReturn.bind(this)} onChange={this.updateLayerGroupFilter.bind(this)} />
-                  <button className="btn" disabled={this.state.query.length ? false : true} onClick={this.filter}>Go</button>
+                  <input placeholder="Search layers" type="text" name="search_layers" value={this.state.query} onKeyUp={this.handleReturn.bind(this)} onChange={this.updateLayerGroupFilter.bind(this)}/>
+                  <button className="btn" disabled={this.state.query.length ? false : true} onClick={this.filter}>Go
+                  </button>
                 </div>
               </div>
 
-              {highlightedLayers.length>0 && (
+              {highlightedLayers.length > 0 && (
                 <div className="layers">
                   <div className="section-title">
                     <h2>Highlighted layers</h2>
@@ -88,7 +89,7 @@ import Layer from '../sources/layer';
                 </div>
               )}
 
-              {layerDirectoryLayers.length>0 && (
+              {layerDirectoryLayers.length > 0 && (
                 <div className="secondary-layers">
                   <div className="section-title">
                     <h2>Layer directory</h2>
@@ -100,9 +101,13 @@ import Layer from '../sources/layer';
                 </div>
               )}
 
+              {layerDirectoryLayers.length === 0 && highlightedLayers.length === 0 && (
+                <div className="no-results">There are no layers which match your search.</div>
+                )}
+
               {/*{Array(this.state.total_pages).fill().map((_,i)=>i+1).map((p, i)=><div key={`layer-group-page-${i}`}>{p}</div>)}*/}
 
-              {this.props.layersStore.activeLayerGroups.length>0 &&
+              {this.props.layersStore.activeLayerGroups.length > 0 &&
               <div className="confirm">
                 <Link to="/map" className="btn" onClick={this.checkRestoreTray.bind(this)}>I'm done!</Link>
               </div>
