e9829a3c83450f27758895fbe54da77bfba52f06
Paul Hendrick <paul@errorstudio.co.uk>
2019-08-09 14:40:10 +0100

    fix colour picker button is-current class

--- src/app/frontend/packs/maptools/components/polygon_container.js
+++ src/app/frontend/packs/maptools/components/polygon_container.js
@@ -25,6 +25,8 @@ export default class PolygonContainer extends React.Component {
             this.polygonElement = element;
             return element;
         };
+
+        this.popupRef = React.createRef();
     }
 
     savePolygon(event) {
@@ -40,6 +42,16 @@ export default class PolygonContainer extends React.Component {
         const {colour} = event.currentTarget.dataset;
         const style = getStyle(colour);
 
+        const previousColour = event.target.parentElement.querySelector(".is-current");
+        if( previousColour ) {
+            previousColour.classList.remove("is-current");
+        }
+
+        event.currentTarget.classList.add("is-current");
+
+        // // we cant use setState here as React draw loses its ref to the leaflet dom object which means we can't edit
+        // // shapes once we change local state. instead, fetch the leaflet element and manually update its state,
+        // // then set the button as is-active...
         this.polygonElement.leafletElement.properties = {...this.polygonElement.leafletElement.properties, colour: colour};
         this.polygonElement.leafletElement.setStyle(style);
         this.savePolygon();
@@ -49,25 +61,35 @@ export default class PolygonContainer extends React.Component {
         const coords = this.props.feature.geometry.coordinates[0].toJS().map((lnglat) => [lnglat[1], lnglat[0]]);
 
         const polygonClicked = event => {
-            window.t = event.target;
-            console.log(event.target);
             return false;
         };
 
-        const popup = <Popup autoClose={true} closeOnClick={false} autoOpen={false}>
-            <div className="m-color-picker" feature={this.props.feature}>
-                <button onClick={this.setColour.bind(this)} data-colour='black'>Black</button>
-                <button onClick={this.setColour.bind(this)} data-colour='blue'>Blue</button>
-                <button onClick={this.setColour.bind(this)} data-colour='blue-hatched'>Blue hatched</button>
-                <button onClick={this.setColour.bind(this)} data-colour='red-soft'>Soft red</button>
-                <button onClick={this.setColour.bind(this)} data-colour='red-hatched'>Red hatched</button>
-                <button onClick={this.setColour.bind(this)} data-colour='red'>Red</button>
-                <button onClick={this.setColour.bind(this)} data-colour='yellow'>Yellow</button>
-                <button onClick={this.setColour.bind(this)} data-colour='yellow-hatched'>Yellow hatched</button>
-                <button onClick={this.setColour.bind(this)} className="unknown" data-colour='unknown'>Too difficult to tell</button>
+        const isCurrent = (buttonClass) => {
+            return `${buttonClass} ${buttonClass===this.props.feature.properties.colour ? 'is-current' : ''}`
+        };
 
-                <hr/>
-                <button className="done-button" onClick={this.savePolygon.bind(this)}>Done</button>
+        const setPopupButtonClass = () => {
+            const popup = document.querySelector('.m-color-picker');
+            const currentButton = popup.querySelector('button.is-current');
+
+            if( currentButton ) {
+                currentButton.classList.remove('is-current');
+            }
+
+            popup.querySelector(`button.${this.polygonElement.leafletElement.properties.colour}`).classList.add('is-current');
+        };
+
+        const popup = <Popup autoClose={true} closeOnClick={false} autoOpen={false} ref={this.popupRef} onOpen={setPopupButtonClass}>
+            <div className="m-color-picker" feature={this.props.feature}>
+                <button onClick={this.setColour.bind(this)} className={'black'} data-colour='black'>Black</button>
+                <button onClick={this.setColour.bind(this)} className={'blue'} data-colour='blue'>Blue</button>
+                <button onClick={this.setColour.bind(this)} className={'blue-hatched'} data-colour='blue-hatched'>Blue hatched</button>
+                <button onClick={this.setColour.bind(this)} className={'red-soft'} data-colour='red-soft'>Soft red</button>
+                <button onClick={this.setColour.bind(this)} className={'red-hatched'} data-colour='red-hatched'>Red hatched</button>
+                <button onClick={this.setColour.bind(this)} className={'red'} data-colour='red'>Red</button>
+                <button onClick={this.setColour.bind(this)} className={'yellow'} data-colour='yellow'>Yellow</button>
+                <button onClick={this.setColour.bind(this)} className={'yellow-hatched'} data-colour='yellow-hatched'>Yellow hatched</button>
+                <button onClick={this.setColour.bind(this)} className={'unknown'} data-colour='unknown'>Too difficult to tell</button>
             </div>
         </Popup>;
 
