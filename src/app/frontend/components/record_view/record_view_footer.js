f7c1c16d68ec6620baf9d6e2888a832c3ea49094
Paul Hendrick <paul@error.agency>
2019-11-08 17:02:41 +0000

    zoom the map and close the modal when viewing a record on the map

--- src/app/frontend/components/record_view/record_view_footer.js
+++ src/app/frontend/components/record_view/record_view_footer.js
@@ -13,26 +13,28 @@ import {appendQueryString} from '../../helpers/modals';
    * hide the currently visible record and pan to its position on the map
    */
   showOnMap() {
+    // this.props.trayViewStore.trayLocked = true;
+
     this.props.mapViewStore.panTo(this.props.trayViewStore.record.lat, this.props.trayViewStore.record.lng, 18);
     const card = this.props.trayViewStore.cards.get(`record_${this.props.trayViewStore.record_id}`);
     if( card ) {
       card.highlighted = true;
     }
 
-    this.props.trayViewStore.record_id = null;
+    this.props.trayViewStore.record = null;
 
-    const matches = this.props.router.location.pathname.match(/^(\/map\/collections\/\d+)\/records/);
+    // const matches = this.props.router.location.pathname.match(/^(\/map\/collections\/\d+)\/records/);
 
-    if( matches && matches.length>1 ) {
-      this.props.router.push(matches[1]);
-    }else {
-      this.props.router.push('/map');
-    }
+    // if( matches && matches.length>1 ) {
+    //   this.props.router.push(matches[1]);
+    // }else {
+    //   this.props.router.push('/map');
+    // }
   }
 
   render() {
     const reportLinkPath = appendQueryString(this.props.location, [{key: 'reportRecord', value: true}]);
-
+    
     return <Fragment>
       <div className="footer">
         <div className="attribution">
@@ -40,21 +42,16 @@ import {appendQueryString} from '../../helpers/modals';
             <li><h4>Created:</h4> {this.props.trayViewStore.record.created_at}</li>
           </ul>
         </div>
+        
         <div className="footer-actions">
-          {/*<button className="contact-owner">Contact owner</button>*/}
-          {/*<button className="flag">Report this record</button>*/}
-
           <button onClick={this.showOnMap.bind(this)}>See on map</button>
-
           <NavLink to={`${this.props.router.location.pathname}?${reportLinkPath}`}>Report this record</NavLink>
-
           {
             this.props.trayViewStore.record.user_can_edit_record &&
             <NavLink to={`/map?editRecord=${this.props.trayViewStore.record.id}`} className="edit">Edit</NavLink>
           }
         </div>
       </div>
-
     </Fragment>
   }
 }
