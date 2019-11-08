5ed2762edfbda7c83e95b55d953e4b6523aaf5b2
seanhawkridge <sean.hawkridge@alliants.com>
2019-11-08 15:14:24 +0000

    Images for related records

--- src/app/frontend/components/record_view/record_view_related_record.js
+++ src/app/frontend/components/record_view/record_view_related_record.js
@@ -2,12 +2,13 @@ import React,{Component, Fragment} from 'react';
 import {observer} from "mobx-react";
 import RecordViewComponentState from './record_view_component_state';
 import {Link} from 'react-router-dom';
+import Img from 'react-image';
 
 @observer class RecordViewRelatedRecord extends Component {
   containerClasses() {
     const number = this.props.record.attributes.title.split('').map((c) => {return c.charCodeAt() }).reduce((a,b) => a + b, 0) % 10;
-    if (this.props.record.image) {
-      return "";
+    if (this.props.record.attributes.image) {
+      return "m-record-card";
     } else {
       return `m-record-card placeholder-${number}`;
     }
@@ -17,6 +18,16 @@ import {Link} from 'react-router-dom';
     return <div className={this.containerClasses()}>
       <Link to={`/map?record=${this.props.record.attributes.id}`} onClick={() => this.props.trayViewStore.record_id = this.props.record.id}>
         <div className="wrapper">
+        {
+            this.props.record.attributes.image &&
+              <div className="image">
+                {
+                  <Fragment>
+                    <Img src={this.props.record.attributes.image.url}/>
+                  </Fragment>
+                }
+              </div>
+          }
           <div className="text-content">
             <h1>
               {this.props.record.attributes.title}
