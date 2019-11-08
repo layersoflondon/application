7c480a13f0f515bc364945d52e4aff32f241494a
Paul Hendrick <paul@error.agency>
2019-11-08 17:03:03 +0000

    close the record view when clicking user link

--- src/app/frontend/components/record_view/record_view_meta.js
+++ src/app/frontend/components/record_view/record_view_meta.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {observer} from "mobx-react";
 import RecordViewComponentState from './record_view_component_state';
-import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 @observer class RecordViewMeta extends Component {
   constructor(props) {
@@ -9,6 +9,11 @@ import {NavLink} from 'react-router-dom';
   }
 
   render() {
+    const clearRecord = () => {
+      this.props.mapViewStore.recordModal = false;
+      this.props.trayViewStore.record = null;
+    }
+
     let author_prefix = '';
     if( this.props.trayViewStore.record.added_by_student ) {
       author_prefix = <span><strong>{this.props.trayViewStore.record.user.student_name}</strong>, a student of </span>
@@ -18,7 +23,12 @@ import {NavLink} from 'react-router-dom';
       <div className="dates">
         <span className="date start-date">{this.props.trayViewStore.record.display_date_from}</span>
       </div>
-      <div className="creator">By {author_prefix}<NavLink to={`/map/users/${this.props.trayViewStore.record.user.id}`}>{this.props.trayViewStore.record.user.name}</NavLink></div>
+      <div className="creator">
+        By {author_prefix}
+        <Link to={`/map/users/${this.props.trayViewStore.record.user.id}`} onClick={clearRecord}>
+          {this.props.trayViewStore.record.user.name}
+        </Link>
+      </div>
     </div>
   }
 }
