e1336123a1778389ea0af95b1db28896b74ad897
Paul Hendrick <paul@error.agency>
2019-11-08 17:04:06 +0000

    dont lose tray state by just updating the record id in the query

--- src/app/frontend/components/record_view/record_view_related_record.js
+++ src/app/frontend/components/record_view/record_view_related_record.js
@@ -1,9 +1,11 @@
 import React,{Component, Fragment} from 'react';
-import {observer} from "mobx-react";
+import {observer, inject} from "mobx-react";
 import RecordViewComponentState from './record_view_component_state';
 import {Link} from 'react-router-dom';
 import Img from 'react-image';
+import {openModalLink} from '../../helpers/modals';
 
+@inject('router')
 @observer class RecordViewRelatedRecord extends Component {
   containerClasses() {
     const number = this.props.record.attributes.title.split('').map((c) => {return c.charCodeAt() }).reduce((a,b) => a + b, 0) % 10;
@@ -15,8 +17,10 @@ import Img from 'react-image';
   }
 
   render() {
+    const url = openModalLink(this.props.router.location, {key: 'record', value: this.props.record.attributes.id});
+    
     return <div className={this.containerClasses()}>
-      <Link to={`/map?record=${this.props.record.attributes.id}`} onClick={() => this.props.trayViewStore.record_id = this.props.record.id}>
+      <Link to={url}>
         <div className="wrapper">
         {
             this.props.record.attributes.image &&
