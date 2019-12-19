4eecbb1bccc16ab6913b1bc5b026ea7b2eb6edf6
Andy Wilkinson <andy@error.agency>
2019-12-19 16:45:11 +0000

    rechecked fixes

--- src/app/frontend/components/error_boundary.js
+++ src/app/frontend/components/error_boundary.js
@@ -1,6 +1,7 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {inject} from "mobx-react";
 import {Link, withRouter} from 'react-router-dom';
+
 @withRouter
 export default class ErrorBoundary extends Component {
   constructor(props) {
@@ -11,27 +12,25 @@ export default class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     this.setState({hasError: true, errorInfo: info});
 
-    if(typeof Rollbar !== "undefined") {
+    if (typeof Rollbar !== "undefined") {
       Rollbar.error(error);
 
-      if( window.__USER_PRESENT ) {
+      if (window.__USER_PRESENT) {
         Rollbar.configure({payload: {person: window.__USER}});
       }
     }
   }
 
   render() {
-    if( this.state.hasError ) {
+    if (this.state.hasError) {
       return <div className='m-overlay'>
 
-        <div className="m-add-record">
+        <div className="m-overlay-error">
           <h1>There was an error</h1>
-
-          <p>
-            We're really sorry - the website encountered an error. It's possible this is something temporary so it's worth trying again, or if you're having problems please <a href="/contact-us">get in touch</a> and let us know what you were doing when it broke. Our technical team has been notified.
-          </p>
-
-          <p><Link to="/map">Go back to the map</Link></p>
+          <p>We're really sorry - the website encountered an error. It's possible this is something temporary so it's
+            worth trying again, or if you're having problems please <a href="/contact-us">get in touch</a> and let us
+            know what you were doing when it broke. Our technical team has been notified.</p>
+          <p><Link className="button" to="/map">Go back to the map</Link></p>
         </div>
       </div>
     }
--- src/app/frontend/components/tray/collections_index.js
+++ src/app/frontend/components/tray/collections_index.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
 import Card from '../card';
 import TrayHeader from '../tray_header';
@@ -14,28 +14,33 @@ export default class TrayCollectionsIndex extends Component {
 
   render() {
     const mainResults = this.props.trayViewStore.mainResults.values().map((result) => {
-      return <Card key={`record_${result.id}`} card={result} trayViewStore={this.props.trayViewStore} mapViewStore={this.props.mapViewStore} />
+      return <Card key={`record_${result.id}`} card={result} trayViewStore={this.props.trayViewStore} mapViewStore={this.props.mapViewStore}/>
     });
-    
+
     return <React.Fragment>
-      <TrayHeader 
-        title="Results for your collection search" 
+
+      <TrayHeader
+        title="Results for your collection search"
         // subtitle={this.props.trayViewStore.collection.title} 
         metaDescription={`Collections index`}
-        metaData={`Collection, ${pluralize('collection', this.props.trayViewStore.mainResults.size, true)}`} 
+        metaData={`Collection, ${pluralize('collection', this.props.trayViewStore.mainResults.size, true)}`}
         closePath={`/map`}
       />
-      
-      <div className="m-tray-records-list">
-        {
-          this.props.trayViewStore.mainResults.size>0 && 
-          <React.Fragment>
-            <h1>{this.props.title}</h1>
-            {mainResults}
-          </React.Fragment>
-        }
-      </div>
-      
+
+      {
+        this.props.title &&
+        <div className="m-tray-subtitle-area">
+          <h1>{this.props.title}</h1>
+        </div>
+      }
+
+      {
+        this.props.trayViewStore.mainResults.size > 0 &&
+        <div className="m-tray-records-list">
+          {mainResults}
+        </div>
+      }
+
     </React.Fragment>
   }
 }
