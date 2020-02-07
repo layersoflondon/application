7410599463e4b5648be3fd262f3369d037f83873
seanhawkridge <sean@error.agency>
2020-02-07 14:18:23 +0000

    Add LayerCategory and LayerTerm to ES

--- src/app/chewy/field_definitions/layer.rb
+++ src/app/chewy/field_definitions/layer.rb
@@ -13,6 +13,29 @@ module FieldDefinitions
       field :date_to, type: 'date'
       field :layer_type, type: 'keyword'
       field :layer_data, type: 'object'
+
+      field :layer_terms, type: :nested, value: -> {
+        layer_terms.collect do |layer_term|
+          {
+            id: layer_term.id,
+            name: layer_term.name,
+          }
+        end
+      }
+        
+      field :layer_categories, type: :nested, value: -> {
+        layer_categories.collect do |layer_category|
+          {
+            id: layer_category.id,
+            name: layer_category.name,
+            layer_terms: layer_category.layer_terms.select{|t| layer_terms.include?(t)}.collect do |layer_term|
+              {
+                id: layer_term.id, name: layer_term.name, layer_category_id: layer_term.layer_category_id
+              }
+            end
+          }
+        end
+      }
     end
   end
 end
