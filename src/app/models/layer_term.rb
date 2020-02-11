1c54feea8527b840c2f881d05b42317b095d0fbe
seanhawkridge <sean@error.agency>
2020-02-11 10:06:41 +0000

    Add Layer terms & categories

--- src/app/models/layer_term.rb
+++ src/app/models/layer_term.rb
@@ -1,5 +1,5 @@
 class LayerTerm < ApplicationRecord
-  has_many :layer_layer_terms
+  has_many :layer_layer_terms, dependent: :destroy
   has_many :layers, through: :layer_layer_terms
   belongs_to :layer_category
 end
--- src/db/migrate/20200131130516_create_layer_terms.rb
+++ src/db/migrate/20200131130516_create_layer_terms.rb
@@ -2,7 +2,7 @@ class CreateLayerTerms < ActiveRecord::Migration[5.2]
   def change
     create_table :layer_terms do |t|
       t.string :name
-      t.references :layer_category, foreign_key: true
+      t.references :layer_category
 
       t.timestamps
     end
