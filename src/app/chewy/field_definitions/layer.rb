7c9f67b166a7320ae2db4c2553870ea13a2742b2
Ed Jones <ed@error.agency>
2020-03-12 17:00:51 +0000

    add analyzers to layer index schema

--- src/app/chewy/field_definitions/layer.rb
+++ src/app/chewy/field_definitions/layer.rb
@@ -4,10 +4,9 @@ module FieldDefinitions
 
     included do
       field :id, type: 'integer'
-      field :title, type: 'text'
-      field :name, type: 'text'
-      field :description, type: 'text'
-      field :credit, type: 'text'
+      field :name, type: 'text', analyzer: :english
+      field :description, type: 'text', analyzer: :english
+      field :credit, type: 'text', analyzer: :english
       field :pin, type: 'geo_point', value: ->{ {lat: lat||0, lon: lng||0} }
       field :date_from, type: 'date'
       field :date_to, type: 'date'
