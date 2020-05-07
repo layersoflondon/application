e7d19515eb44417ceb48b732a5e6fd10241ec846
Ed Jones <ed@error.agency>
2020-05-07 14:10:14 +0100

    Add first and last names to profile edit pagew

--- src/app/controllers/users/registrations_controller.rb
+++ src/app/controllers/users/registrations_controller.rb
@@ -87,7 +87,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   def account_update_params
-    params.require(:user).permit(:description, :email, :password, :password_confirmation, :current_password, :avatar, :title)
+    params.require(:user).permit(:description, :email, :password, :password_confirmation, :current_password, :avatar, :title, :first_name, :last_name)
   end
 
   def after_update_path_for(resource)
