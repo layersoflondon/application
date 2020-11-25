9d108802ff3ea10ebb871360fd0bdeec82b8346a
Ed Jones <ed@error.agency>
2020-11-25 16:13:13 +0000

    Don't iterate over layers in the migration, because the association is missing in the code

--- src/db/migrate/20201125150101_create_layer_group_layers.rb
+++ src/db/migrate/20201125150101_create_layer_group_layers.rb
@@ -7,11 +7,11 @@ class CreateLayerGroupLayers < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    puts "Migrating layer group IDs to join table"
-    Layer.all.each do |layer|
-      puts "Layer #{layer.id}"
-      LayerGroupLayer.create(layer: layer, layer_group: layer.layer_group)
-    end
+    # puts "Migrating layer group IDs to join table"
+    # Layer.all.each do |layer|
+    #   puts "Layer #{layer.id}"
+    #   LayerGroupLayer.create(layer: layer, layer_group: layer.layer_group)
+    # end
 
     remove_column :layers, :layer_group_id
   end
@@ -19,12 +19,12 @@ class CreateLayerGroupLayers < ActiveRecord::Migration[5.2]
   def down
     add_column :layers, :layer_group_id
 
-    puts "Migration layer group IDs back into layers"
-    LayerGroupLayer.all.each do |lgl|
-      puts "Layer ID #{lgl.layer.id}"
-    #   we can't associate the layer to more than one layer group on the 'down' migration so we'll just do one.
-    lgl.layer.update_column(:layer_group_id, lgl.layer.id) unless lgl.layer.layer_group_id.present?
-    end
+    # puts "Migration layer group IDs back into layers"
+    # LayerGroupLayer.all.each do |lgl|
+    #   puts "Layer ID #{lgl.layer.id}"
+    # #   we can't associate the layer to more than one layer group on the 'down' migration so we'll just do one.
+    # lgl.layer.update_column(:layer_group_id, lgl.layer.id) unless lgl.layer.layer_group_id.present?
+    # end
 
     drop_table :layer_group_layers
   end
