1320f457c58f5c2c2a3d8fd9612b10a3a2536706
Ed Jones <ed@error.agency>
2020-11-25 13:46:10 +0000

    Add env var options for elasticsearch and activestorage settings

--- src/config/initializers/chewy.rb
+++ src/config/initializers/chewy.rb
@@ -1,6 +1,6 @@
 Chewy.strategy(:active_job)
 Chewy.logger = Logger.new(STDOUT)
 Chewy.settings = {
-  prefix: Rails.env,
+  prefix: ENV['ELASTIC_SEARCH_PREFIX'] || Rails.env,
   host: Rails.env.development? ? "elasticsearch:9200" : "db01.lol:9200"
 }
