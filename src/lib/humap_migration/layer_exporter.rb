8aa5150d74c80bcec0a85fef87d1806ef5a9a1d0
Ed Jones <ed@humap.me>
2021-10-03 14:12:46 +0100

    add a load of conditional checks for different ways of getting location of tiles

--- src/lib/humap_migration/layer_exporter.rb
+++ src/lib/humap_migration/layer_exporter.rb
@@ -68,24 +68,37 @@ module HumapMigration
           # The layer needs to have centroid (lat/lng), southwest extent, northeast extent, min and max zooms
           #
           #
-          zoomlevels = list_zoomlevels(layer)
-          x_folders = list_x_folders(layer, zoom: 13)
-          raise ArgumentError, "No x folders at zoom level 13 - possibly a bad URL?" unless x_folders.any?
-          y_files_min_x = list_y_files(layer, x: x_folders.min)
-          raise ArgumentError, "No y files for x: #{x_folders.min}" unless y_files_min_x.any?
-          y_files_max_x = list_y_files(layer, x: x_folders.max)
-          raise ArgumentError, "No y files for x: #{x_folders.max}" unless y_files_max_x.any?
-          sw_extent = get_lat_lng_for_number(x_folders.min, y_files_min_x.max + 1, 13)
-          ne_extent = get_lat_lng_for_number(x_folders.max+1, y_files_max_x.min, 13)
-          
+          #
+
+          if layer.layer_data["url"].match(%r{tiles.layersoflondon.org})
+            # Get the extent and zoom from the tiles on s3
+            sw_extent, ne_extent, min_zoom, max_zoom = parse_s3_tileset(layer)
+          elsif layer.layer_data["url"].match(%r{georeferencer.com})
+            #   Get the extent and zoom from the tilejson at map.json
+            tilejson = JSON.parse(open(layer.layer_data["url"].gsub("/map/{z}/{x}/{y}.png", "/map.json").strip).read)
+            min_zoom = tilejson["minzoom"]
+            max_zoom = tilejson["maxzoom"]
+            bounds = tilejson["bounds"]
+            sw_extent = {lat: bounds[1], lon: bounds[0]}
+            ne_extent = {lat: bounds[3], lon: bounds[2]}
+          elsif layer.layer_data["url"].match(%r{mapseries-tilesets.s3.amazonaws.com})
+            #   Get the extent and zoom from the json at metadata.json
+            tilejson = JSON.parse(open(layer.layer_data["url"].gsub("/{z}/{x}/{y}.png","/metadata.json").strip).read)
+            min_zoom = tilejson["minzoom"].to_i
+            max_zoom = tilejson["maxzoom"].to_i
+            bounds = tilejson["bounds"].split(",").collect(&:to_f)
+            sw_extent = {lat: bounds[1], lon: bounds[0]}
+            ne_extent = {lat: bounds[3], lon: bounds[2]}
+          end
+
           centroid_lat = sw_extent[:lat] + ((ne_extent[:lat] - sw_extent[:lat]) / 2)
           centroid_lon = sw_extent[:lon] + ((ne_extent[:lon] - sw_extent[:lon]) / 2)
           
 
           layer_data.merge!({
                               metadata: layer.layer_data.merge({
-                                min_zoom: zoomlevels.min,
-                                max_zoom: zoomlevels.max,
+                                min_zoom: min_zoom,
+                                max_zoom: max_zoom,
                                 sw_extent: sw_extent,
                                 ne_extent: ne_extent,
                                 centroid: {lat: centroid_lat, lon: centroid_lon}
@@ -134,6 +147,23 @@ module HumapMigration
       result = ActiveStorage::Blob.service.client.client.list_objects_v2(bucket: "tiles.layersoflondon.org", prefix: prefix, delimiter: "/")
       result.contents.collect(&:key).collect {|k| k.split("/").last}.select {|k| k.match(%{\.[A-z]{3,4}$})}.collect {|k| k.split(".")[0..-2].join(".").try(:to_i) || 0}.sort
     end
+
+    def parse_s3_tileset(layer)
+      zoomlevels = list_zoomlevels(layer)
+      x_folders = list_x_folders(layer, zoom: 13)
+      raise ArgumentError, "No x folders at zoom level 13 - possibly a bad URL?" unless x_folders.any?
+      y_files_min_x = list_y_files(layer, x: x_folders.min)
+      raise ArgumentError, "No y files for x: #{x_folders.min}" unless y_files_min_x.any?
+      y_files_max_x = list_y_files(layer, x: x_folders.max)
+      raise ArgumentError, "No y files for x: #{x_folders.max}" unless y_files_max_x.any?
+      sw_extent = get_lat_lng_for_number(x_folders.min, y_files_min_x.max + 1, 13)
+      ne_extent = get_lat_lng_for_number(x_folders.max+1, y_files_max_x.min, 13)
+
+      [sw_extent, ne_extent, zoomlevels.min, zoomlevels.max]
+
+    end
+
+
 
 
   end
