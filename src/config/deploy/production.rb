475ac38b4f52d85d8504076f5140739ddd8cdbbc
Ed Jones <ed@humap.me>
2021-11-17 11:30:16 +0000

    fix up url after move to Humap

--- src/config/deploy/production.rb
+++ src/config/deploy/production.rb
@@ -15,9 +15,9 @@ set :base_domain, "prelaunch.error.agency"
 set :prelaunch_domain, ->{"#{fetch(:primary_domain)}.#{fetch(:base_domain)}"}
 
 #domains which this site will answer to (i.e. not redirect)
-set :site_domains, [fetch(:primary_domain)]
+set :site_domains, ["old.layersoflondon.org"]
 #redirects domains to the primary domain as a 301
-set :domain_redirects, %w(alpha.layersoflondon.org beta.layersoflondon.org prototype.layersoflondon.org layersoflondon.org)
+set :domain_redirects, %w()
 
 #rewrites in nginx format - useful for specifying hard-coded urls for redirection after launch
 
@@ -40,12 +40,12 @@ set :passenger_port, 8002
 set :passenger_max_pool_size, 100
 set :passenger_min_instances, 25
 
-#SSL settings
-set :ssl_required, true
-set :ssl_dir, File.join(File.dirname(__FILE__),"ssl")
-set :ssl_cert, "layersoflondon.org.crt"
-set :ssl_key, "layersoflondon.org.key.gpg" #this should be a gpg-encrypted key
-set :ssl_dh, "dhparams.pem.gpg" #this should be a gpg-encrypted key
+# #SSL settings
+# set :ssl_required, true
+# set :ssl_dir, File.join(File.dirname(__FILE__),"ssl")
+# set :ssl_cert, "layersoflondon.org.crt"
+# set :ssl_key, "layersoflondon.org.key.gpg" #this should be a gpg-encrypted key
+# set :ssl_dh, "dhparams.pem.gpg" #this should be a gpg-encrypted key
 
 set :cron_scripts,  {
   d: [
@@ -53,6 +53,10 @@ set :cron_scripts,  {
     File.join(File.expand_path(File.dirname(__FILE__)), "templates", "georeferencer_cache.sh.erb")
   ]
 }
+
+set :basic_auth_required, true
+set :basic_auth_username, 'lol'
+set :basic_auth_password, 'oldlol'
 
 
 set :upstart_scripts, [
